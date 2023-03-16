854c603c782628fe414d651dc193d51d99720f8e
Johnny McGown <jmac3292@gmail.com>
2023-03-15 22:12:27 -0600

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const noteData = require("./db/db.json");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "public")));
 
