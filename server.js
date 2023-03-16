bcdb2636115e907d736f2822885134e15432a862
Johnny McGown <jmac3292@gmail.com>
2023-03-15 19:47:38 -0600

    Looked at class actiity build out first set of server.js code

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const noteData = require("./db/db.json");
+const app = express();
+const PORT = 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is listening on PORT: ${PORT}`);
+});
