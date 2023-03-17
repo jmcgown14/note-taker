5cbc88687d3a6c3f36571699a4efda49a6b2abd2
Johnny McGown <jmac3292@gmail.com>
2023-03-16 20:22:19 -0600

    trying out some different save routes

--- server.js
+++ server.js
@@ -18,6 +18,63 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+// Function to save new notes
+function createNewNotes(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray))
+  notesArray = [];
+
+  if (notesArray.length === 0)
+  notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  FileSystem.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+
+// The POST mehtod used to bring it to the backend
+app.post("/api/notes", (req, res) => {
+  const newNote = createNewNotes(req.body, notesData);
+  res.json(newNote);
+});
+
 app.listen(PORT, () => {
   console.log(`The server is listening on PORT: ${PORT}`);
 });
+
+// potential other way to do save route
+// app.post("/api/notes", (req, res) => {
+//   const newNote = req.body;
+//   newNote.id = Date.now().toString(); // add a unique ID to the new note
+//   noteData.push(newNote);
+//   fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) => {
+//     if (err) throw err;
+//     res.json(newNote);
+//   });
+// });
+
+// document.getElementById("save-note").addEventListener("click", function(event) {
+//   event.preventDefault(); // prevent the default form submit behavior
+//   const noteTitle = document.getElementById("note-title").value;
+//   const noteText = document.getElementById("note-text").value;
+//   const newNote = { title: noteTitle, text: noteText };
+//   fetch("/api/notes", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(newNote)
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data); // log the newly created note to the console
+//     // do something with the newly created note, like add it to the note list
+//   })
+//   .catch(error => console.error(error));
+// });
